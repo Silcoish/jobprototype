4b6575745e62f8fdfb10095d86ad33ed735b6305
silcoish <silcoish@gmail.com>
2016-02-16 12:57:58 +1000

    Populates jobs and rotates and wraps

--- JobCirclePrototype/Assets/JobsCircle.cs
+++ JobCirclePrototype/Assets/JobsCircle.cs
@@ -3,46 +3,52 @@ using System.Collections.Generic;
 
 public class JobsCircle : MonoBehaviour {
 
-	public float rotationAmount = 7.0f;
 	private List<GameObject> iconPositions;
 	public List<GameObject> spawnedObjects;
 	public Sprite[] unlockedJobs;
 
 	private int currentJobIndex = 0;
 
+    public float radius = 3.5f;
     public int iconAmounts = 10;
     public float degreeOffset = 7;
 
+    private float currentRotation = 0;
+
 	void Start()
 	{
-        for(int i = 0; i < iconAmounts; i++)
-        {
-            GameObject go = new GameObject("yo");
-            float angle = 180 + (0.5f * iconAmounts * degreeOffset) - ((iconAmounts - i) * degreeOffset);
-            print(angle);
-            Vector2 newPos = new Vector2(transform.position.x + Mathf.Cos(Mathf.Deg2Rad * angle) * 3.5f, transform.position.y + Mathf.Sin(Mathf.Deg2Rad * angle) * 3.5f);
-            go.transform.position = newPos;
-        }
-		//PopulateList();
-	}
+        currentRotation = transform.rotation.z;
+
+        iconPositions = new List<GameObject>();
+        spawnedObjects = new List<GameObject>();
+
+        PopulateList();
+    }
 
 	void Update()
 	{
 		checkForRotation();
 	}
 
-	private void PopulateList() 
-	{
-		for (int i = 0; i < iconPositions.Count; i++) 
-		{
-            GameObject go = new GameObject("iconObject");
-            go.AddComponent<SpriteRenderer>();
-            go.GetComponent<SpriteRenderer>().sprite = GetNextJobIcon();
-            go.transform.position = iconPositions[i].transform.position;
-            go.transform.parent = transform;
-            spawnedObjects.Add(go);
-		}
-	}
+	private void PopulateList()
+    {
+        for (int i = 0; i < iconAmounts; i++)
+        {
+            GameObject go = new GameObject("spawnPos");
+            float angle = 180 + (0.5f * iconAmounts * degreeOffset) - ((iconAmounts - i) * degreeOffset);
+            print(angle);
+            Vector2 newPos = new Vector2(transform.position.x + (Mathf.Cos(Mathf.Deg2Rad * angle) * radius), transform.position.y + (Mathf.Sin(Mathf.Deg2Rad * angle) * radius));
+            go.transform.position = newPos;
+            iconPositions.Add(go);
+
+            GameObject go2 = new GameObject("spawnSprite");
+            go2.transform.position = iconPositions[i].transform.position;
+            go2.transform.parent = transform;
+            SpriteRenderer sr = go2.AddComponent<SpriteRenderer>();
+            sr.sprite = GetNextJobIcon();
+            spawnedObjects.Add(go2);
+        }
+    }
 
 
 	private Sprite GetNextJobIcon()
@@ -65,10 +71,20 @@ public class JobsCircle : MonoBehaviour {
 	private void checkForRotation()
 	{
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			gameObject.transform.Rotate(new Vector3(0f, 0f, -rotationAmount));
+			gameObject.transform.Rotate(new Vector3(0f, 0f, -degreeOffset));
+            GameObject topObject = spawnedObjects[0];
+            spawnedObjects.Remove(topObject);
+            spawnedObjects.Add(topObject);
+
+            topObject.transform.position = iconPositions[iconPositions.Count - 1].transform.position;
 		}
 		else if(Input.GetKeyDown(KeyCode.RightArrow)) {
-			gameObject.transform.Rotate(new Vector3(0f, 0f, rotationAmount));
+			gameObject.transform.Rotate(new Vector3(0f, 0f, degreeOffset));
+            GameObject endObject = spawnedObjects[spawnedObjects.Count - 1];
+            spawnedObjects.Remove(endObject);
+            spawnedObjects.Insert(0, endObject);
+
+            endObject.transform.position = iconPositions[0].transform.position;
 		}
 	}
 }
